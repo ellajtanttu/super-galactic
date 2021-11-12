46bae93d2729bb8542bc441498a302ac9d49a11f
Ella Tanttu <ellajtanttu@gmail.com>
2021-11-12 11:43:32 -0800

    pass test: first test create object

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -11,17 +11,16 @@
 
 // expect(monster1.level).toBeDefined();
 
-
 import {GlabbaData} from './../src/calculator.js'; // object
 
 describe('GlabbaData', () => {
 
   test('should correctly create a GlabbaData object with ten keys', () => {
-    const testGlabba = new GlabbaData("Gloopdoop", 26, "Magblag", True, "herbivore", 0, [0,0], [0,0], [0,0], [0,0]);
+    const testGlabba = new GlabbaData("Gloopdoop", 26, "Magblag", true, "herbivore", 0, [0,0], [0,0], [0,0], [0,0]);
     expect(testGlabba.name).toEqual("Gloopdoop");
     expect(testGlabba.earthAge).toEqual(26);
     expect(testGlabba.type).toEqual("Magblag");
-    expect(testGlabba.combs).toEqual(True);
+    expect(testGlabba.combs).toEqual(true);
     expect(testGlabba.diet).toEqual("herbivore");
     expect(testGlabba.earthLE).toEqual(0);
     expect(testGlabba.mercData).toEqual([0,0]);
---
+++ src/calculator.js
@@ -0,0 +1,18 @@
+// export class TestObject {
+//   constructor(number) {
+//     this.number = 3;
+//   }
+// }
+
+export function GlabbaData(name, earthAge, type, combs, diet, earthLE, mercData, venData, marsData, jupData) {
+  this.name = name;
+  this.earthAge = earthAge;
+  this.type = type;
+  this.combs = combs;
+  this.diet = diet;
+  this.earthLE = earthLE;
+  this.mercData = mercData;
+  this.venData = venData;
+  this.marsData = marsData;
+  this.jupData = jupData;
+}
